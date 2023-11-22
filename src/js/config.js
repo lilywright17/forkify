79a649765eb7a2c4de2e25377731bf89def860c0
lilywright17 <lilywright93@gmail.com>
2023-11-22 17:15:23 +0000

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,9 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+
+export const API_KEY = 'ab65a253-63e7-4f96-b053-c8921e35e2a0';
+
+export const TIMEOUT_SEC = 10;
+
+export const RES_PER_PAGE = 10;
+
+export const MODAL_CLOSE_SEC = 2.5;
